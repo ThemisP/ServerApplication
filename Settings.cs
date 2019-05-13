69ad9b1ef3dcef5757428bad495c28e42ad734f7
nash <mncubenashe@gmail.com>
2019-05-13 18:38:46 +0100

    Gameday settings set

--- Settings.cs
+++ Settings.cs
@@ -9,8 +9,8 @@ namespace ServerApplication {
         public const int MAX_PLAYERS = 100;
         public const int SEND_BUFFER_SIZE = 4096;
         public const int RECEIVE_BUFFER_SIZE = 4096;
-        public const int MAX_ROOMS = 2;
-        public const int MIN_ROOMS = 1;
+        public const int MAX_ROOMS = 64;
+        public const int MIN_ROOMS = 20;
         public const float MAX_START_TIMER = 300f;
         public const float MAX_GAME_TIMER = 600f;
     }
